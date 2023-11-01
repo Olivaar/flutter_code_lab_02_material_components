e5f8bcc50fbfb4d13de6470f99bbc99f567db8cc
Olivar Neemias Moises Chone Tamele <mocolivarbaloi@gmail.com>
2023-10-31 21:04:13 -0300

    Initial commit

---
+++ lib/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const Color kShrinePink50 = Color(0xFFFEEAE6);
+const Color kShrinePink100 = Color(0xFFFEDBD0);
+const Color kShrinePink300 = Color(0xFFFBB8AC);
+const Color kShrinePink400 = Color(0xFFEAA4A4);
+
+const Color kShrineBrown900 = Color(0xFF442B2D);
+
+const Color kShrineErrorRed = Color(0xFFC5032B);
+
+const Color kShrineSurfaceWhite = Color(0xFFFFFBFA);
+const Color kShrineBackgroundWhite = Colors.white;
+
+const kShrinePurple = Color(0xFF5D1049);
